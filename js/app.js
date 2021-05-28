18ab757cb88c0bd70519d9593299690112fd8c95
unknown <louis.martin-compiegne@hetic.net>
2021-05-28 11:58:10 +0200

    récupération des infos dans le json

--- js/app.js
+++ js/app.js
@@ -2,32 +2,35 @@ const basketArticles = [];
 
 let buttons = document.querySelectorAll('a.addCard');
 
+let url = 'js/products.json';
+let obj = [];
+
+fetch(url)
+    .then(res => res.json())
+    .then(out => obj = out)
+    .then(() => console.log(obj)).catch(err => { throw err });
+
 function addToBasket(item){
+    item.quantity = 1;
     basketArticles.push(item);
 }
 
+// on parcours tout les bouttons acheter pour leur mettre
+// un écouteur d'event
 buttons.forEach(item => {
    item.addEventListener('click', () => {
+       
         let itemId = item.getAttribute('data-id');
+
         if (basketArticles.some(elem => elem.id == itemId)) {
             let index = basketArticles.findIndex(x => x.id === itemId);
             basketArticles[index].quantity += 1;
             console.log('l\'article existe déjà dans le panier');
         } else {
-            let itemName = document.querySelector(`#card-${itemId} h5.card-title`).textContent;
-            let itemPrice = document.querySelector(`#card-${itemId} span.price`).textContent;
-            let itemRef = document.querySelector(`#card-${itemId} span.ref`).textContent; 
-    
-            let newItem = {
-                id: itemId,
-                name: itemName,
-                price: itemPrice,
-                ref: itemRef,
-                quantity: 1
-            };
-           addToBasket(newItem);
+            let index = obj.findIndex(x => x.id === itemId);
+            console.log(obj[index]);
+            addToBasket(obj[index]);
         }
    })
 })
 
-console.log(products);
