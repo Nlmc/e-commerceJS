f3f3ddf4fa830181a006f0d7edf8de0ea5b0741f
hrafDigit <bonjour@achrafreda.fr>
2021-05-28 09:22:20 +0200

    modif app.js avec code de Louis

--- js/app.js
+++ js/app.js
@@ -1,216 +1,32 @@
-var cart = {
+const basketArticles = [];
 
-    // (A) PROPERTIES
-    hPdt: null, // HTML products list
-    hItems: null, // HTML current cart
-    items: {}, // Current items in cart
+let buttons = document.querySelectorAll('a.addCard');
 
-    // (B) LOCALSTORAGE CART
-    // (B2) LOAD CART FROM LOCALSTORAGE
-    load: function () {
-        cart.items = localStorage.getItem("cart");
-        if (cart.items == null) {
-            cart.items = {};
-        }
-        else {
-            cart.items = JSON.parse(cart.items);
-        }
-    },
-    // (B1) SAVE CURRENT CART INTO LOCALSTORAGE
-    save: function () {
-        localStorage.setItem("cart", JSON.stringify(cart.items));
-    },
-    // (B3) EMPTY CART!
-    empty: function () {
-        if (confirm("Empty cart?")) {
-            cart.items = {};
-            localStorage.removeItem("cart");
-            cart.list();
-        }
-    },
+function addToBasket(item){
+    basketArticles.push({
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        ref: item,
+        quantity: 1
+    });
+}
 
-    // (C) INITIALIZE
-    init: function () {
-        // (C1) GET HTML ELEMENTS
-        cart.hPdt = document.getElementById("cart-products");
-        cart.hItems = document.getElementById("cart-items");
+buttons.forEach(item => {
+   item.addEventListener('click', () => {
+        let itemId = item.getAttribute('data-id');
+        let itemName = document.querySelector(`#card-${itemId} h5.card-title`).textContent;
+        let itemPrice = document.querySelector(`#card-${itemId} span.price`).textContent;
+        let itemRef = document.querySelector(`#card-${itemId} span.ref`).textContent; 
 
-        // (C2) DRAW PRODUCTS LIST
-        cart.hPdt.innerHTML = "";
-        let p, item, part;
-        for (let id in products) {
-            // WRAPPER
-            p = products[id];
-            item = document.createElement("div");
-            item.className = "card";
-            cart.hPdt.appendChild(item);
+        let newItem = {
+            id: itemId,
+            name: itemName,
+            price: itemPrice,
+            ref: itemRef
+       };
 
-            // PRODUCT IMAGE
-            part = document.createElement("img");
-            part.src = "images/" + p.img;
-            part.className = "card-img-top";
-            item.appendChild(part);
-
-            // BODY
-            part = document.createElement("div");
-            part.className = "card-body";
-            item.appendChild(part);
-
-            // PRODUCT NAME
-            part = document.createElement("h5");
-            part.innerHTML = p.name;
-            part.className = "card-title";
-            item.appendChild(part);
-
-            // PRODUCT DESCRIPTION
-            part = document.createElement("p");
-            part.innerHTML = p.desc;
-            part.className = "card-text";
-            item.appendChild(part);
-
-            // PRODUCT PRICE
-            part = document.createElement("p");
-            // part.innerHTML = p.price + " €";
-            part.innerHTML = `${p.price} €`;
-            part.className = "card-text";
-            item.appendChild(part);
-
-            // PRODUCT PRICE
-            part = document.createElement("p");
-            part.innerHTML =  "réf. | " + p.ref;
-            part.className = "small";
-            item.appendChild(part);
-
-            // ADD TO CART
-            part = document.createElement("input");
-            part.type = "button";
-            part.value = "Ajouter au panier";
-            part.className = "btn btn-primary";
-            part.onclick = cart.add;
-            part.dataset.id = id;
-            item.appendChild(part);
-        }
-
-        // (C3) LOAD CART FROM PREVIOUS SESSION
-        cart.load();
-
-        // (C4) LIST CURRENT CART ITEMS
-        cart.list();
-    },
-
-    // (D) LIST CURRENT CART ITEMS (IN HTML)
-    list: function () {
-        // (D1) RESET
-        cart.hItems.innerHTML = "";
-        let item, part, pdt;
-        let empty = true;
-        for (let key in cart.items) {
-            if (cart.items.hasOwnProperty(key)) { empty = false; break; }
-        }
-
-        // (D2) CART IS EMPTY
-        if (empty) {
-            item = document.createElement("div");
-            item.innerHTML = "Cart is empty";
-            cart.hItems.appendChild(item);
-        }
-
-        // (D3) CART IS NOT EMPTY - LIST ITEMS
-        else {
-            let p, total = 0, subtotal = 0;
-            for (let id in cart.items) {
-                // ITEM
-                p = products[id];
-                item = document.createElement("div");
-                item.className = "c-item";
-                cart.hItems.appendChild(item);
-
-                // NAME
-                part = document.createElement("div");
-                part.innerHTML = p.name;
-                part.className = "c-name";
-                item.appendChild(part);
-
-                // REMOVE
-                part = document.createElement("input");
-                part.type = "button";
-                part.value = "X";
-                part.dataset.id = id;
-                part.className = "c-del cart";
-                part.addEventListener("click", cart.remove);
-                item.appendChild(part);
-
-                // QUANTITY
-                part = document.createElement("input");
-                part.type = "number";
-                part.value = cart.items[id];
-                part.dataset.id = id;
-                part.className = "c-qty";
-                part.addEventListener("change", cart.change);
-                item.appendChild(part);
-
-                // SUBTOTAL
-                subtotal = cart.items[id] * p.price;
-                total += subtotal;
-            }
-
-            // EMPTY BUTTONS
-            item = document.createElement("input");
-            item.type = "button";
-            item.value = "Empty";
-            item.addEventListener("click", cart.empty);
-            item.className = "c-empty cart";
-            cart.hItems.appendChild(item);
-
-            // CHECKOUT BUTTONS
-            item = document.createElement("input");
-            item.type = "button";
-            item.value = "Checkout - " + "$" + total;
-            item.addEventListener("click", cart.checkout);
-            item.className = "c-checkout cart";
-            cart.hItems.appendChild(item);
-        }
-    },
-
-    // (E) ADD ITEM INTO CART
-    add: function () {
-        if (cart.items[this.dataset.id] == undefined) {
-            cart.items[this.dataset.id] = 1;
-        } else {
-            cart.items[this.dataset.id]++;
-        }
-        cart.save();
-        cart.list();
-    },
-
-    // (F) CHANGE QUANTITY
-    change: function () {
-        if (this.value == 0) {
-            delete cart.items[this.dataset.id];
-        } else {
-            cart.items[this.dataset.id] = this.value;
-        }
-        cart.save();
-        cart.list();
-    },
-
-    // (G) REMOVE ITEM FROM CART
-    remove: function () {
-        delete cart.items[this.dataset.id];
-        cart.save();
-        cart.list();
-    },
-
-
-    // (H) CHECKOUT
-    checkout: function () {
-        // SEND DATA TO SERVER
-        // CHECKS
-        // SEND AN EMAIL
-        // RECORD TO DATABASE
-        // PAYMENT
-        // WHATEVER IS REQUIRED
-        alert("TO DO");
-    }
-};
-window.addEventListener("DOMContentLoaded", cart.init);
+       addToBasket(newItem);
+       console.log(basketArticles);
+   })
+})
